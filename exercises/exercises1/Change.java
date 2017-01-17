cba10521d102fb16193811246e527db4e2a14f79
sjbla <sjbla@Shane-LT.dhcp.wustl.edu>
2017-01-17 15:27:35 -0600

    Add in comments for Change.java

--- exercises/exercises1/Change.java
+++ exercises/exercises1/Change.java
@@ -9,19 +9,23 @@ public class Change {
 		// Below, prompt the user to enter a number of pennies
 		//
 		
+		//Setup user prompt
 		ArgsProcessor ap = new ArgsProcessor(args);
 		
+		//Ask for amount of pennies
 		int change = ap.nextInt("Number of pennies?");
 		
+		//Calculate number of each coin
 		int dollars = change / 100;
 		int quarters = (change - (dollars * 100)) / 25;
 		int dimes = (change - (dollars * 100) - (quarters * 25)) / 10;
 		int nickels = (change - (dollars * 100) - (quarters * 25) - (dimes * 10)) / 5;
 		int pennies = (change - (dollars * 100) - (quarters * 25) - (dimes * 10) - (nickels * 5));
 		
+		//Check to see if the coins added up match the initial value
 		int check = (dollars * 100) + (quarters * 25) + (dimes * 10) + (nickels * 5) + pennies;
 				
-				
+		//Output number of each coin		
 		System.out.print("For " + change + " pennies: " + dollars + " dollars and " + quarters + " quarters and " + dimes + " dimes and " + nickels + " nickels and " + pennies + " pennies " + check );
 		
 		
