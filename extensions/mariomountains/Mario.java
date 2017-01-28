5b22639b574894944b38aa14dfaa76c7e9ce3ee2
sjbla <sjbla@Shane-LT.dhcp.wustl.edu>
2017-01-28 00:26:20 -0600

    Creation of mariomountains package and Mario.java

---
+++ extensions/mariomountains/Mario.java
@@ -0,0 +1,12 @@
+package mariomountains;
+import cse131.ArgsProcessor;
+public class Mario {
+
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		
+		
+
+	}
+
+}
