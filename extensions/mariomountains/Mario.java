4900ef6daa4ee04c34dbf112fccd552dedbb68d9
sjbla <sjbla@Shane-LT.dhcp.wustl.edu>
2017-01-28 00:48:06 -0600

    Changes to Mario.java

--- extensions/mariomountains/Mario.java
+++ extensions/mariomountains/Mario.java
@@ -4,9 +4,15 @@ public class Mario {
 
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
+		int number = ap.nextInt("What level of mountain?");
 		
+		String mario = "";
 		
-
+		for (int x = 0; x < number; x++) {
+			mario = mario + "#";
+			System.out.println(mario);
+		}
+		
 	}
 
 }
