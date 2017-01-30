d1356c11d969db2e1f54d643c42166dd545c1587
sjbla <sjbla@Shane-LT.dhcp.wustl.edu>
2017-01-29 23:33:52 -0600

    Initial changes to OneRepMax.java (Extension 2.2)

---
+++ extensions/onerepmax/OneRepMax.java
@@ -0,0 +1,27 @@
+package onerepmax;
+import cse131.ArgsProcessor;
+public class OneRepMax {
+
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		
+		int percent = 100;
+		int weight = ap.nextInt("What is the current successful bench weight?");
+		double succReps = ap.nextInt("How many successful reps?");
+		int desReps = ap.nextInt("How many desired reps?");
+		
+		double repMax = weight * (1 + (succReps)/30);
+		double fiveReps = repMax * 0.85;
+		
+		System.out.println("One-rep max: " + repMax);
+		System.out.println("Weight for " + desReps + " reps: " + fiveReps);
+		
+		for (int x = 0; x < 10; x++) {
+			percent = (percent - 5);
+			double percentPrint = percent / 100.0;
+			double percentWeight = repMax * percentPrint;
+			System.out.println(percentPrint + "% 1 RM: " + percentWeight);
+		}
+	}
+
+}
