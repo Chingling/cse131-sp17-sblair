f8bef4c65b01b83ec2170c628a3562e740fe6ad8
Chingling <Chingling@Shane-PC.resnet.wustl.edu>
2017-02-11 22:45:28 -0600

    Starting changes to FindTheFrame.java

--- extensions/biofindframe/FindTheFrame.java
+++ extensions/biofindframe/FindTheFrame.java
@@ -55,10 +55,23 @@ public class FindTheFrame {
 		//     named ochre, amber, and opal
 		//     See http://en.wikipedia.org/wiki/Genetic_code#Start.2Fstop_codons
 
+		char ochre[] = {
+				'T','A','A'
+		};
+		char amber[] = {
+				'T','A','G'
+		};
+		char opal[] = {
+				'T','G','A'
+		};
 		
 		
 		// Below, define the Start Codon (Methionine) as an array of char
 		//Start Codon
+		
+		char meth[] = {
+			'A','T','G'	
+		};
 		
 		int ans = -1;  // returned if no appropriate sequences was found
 		
@@ -66,6 +79,153 @@ public class FindTheFrame {
 		// Follow the instructions in the extension write up
 		//
 		
+		char checkA[] = new char[3];
+			
+		//Ref 1
+		int count1 = 0;
+		int position1 = 0;
+		while (position1 < dna.length) {
+			
+			//Look for start codon
+			for (int i = position1; i < dna.length; i = i + 3) {
+				if (i + 2 > dna.length - 1) {
+					i = dna.length;
+				}
+				else {
+					checkA[0] = dna[i];
+					checkA[1] = dna[i + 1]; 
+					checkA[2] = dna[i + 2];
+				}
+				
+					position1 = i + 3;
+					if (checkA[0] == meth[0] && checkA[1] == meth[1] && checkA[2] == meth[2]) {
+						i = dna.length;
+				}
+				System.out.println(position1);	
+			}
+			
+			//Look for stop codon
+			
+			for (int i = position1; i < dna.length; i = i + 3) {
+				if (i + 2 > dna.length - 1) {
+					i = dna.length;
+				}
+				else {
+					checkA[0] = dna[i];
+					checkA[1] = dna[i + 1]; 
+					checkA[2] = dna[i + 2];
+				}
+				position1 = i + 3;
+				if ((checkA[0] == ochre[0] && checkA[1] == ochre[1] && checkA[2] == ochre[2]) || (checkA[0] == amber[0] && checkA[1] == amber[1] && checkA[2] == amber[2]) || (checkA[0] == opal[0] && checkA[1] == opal[1] && checkA[2] == opal[2])) {
+					i = dna.length;
+				}
+				else {
+					count1 = count1 + 1;
+				}
+				System.out.println(position1);	
+			}
+			
+		}
+			
+			
+		//Ref 2
+		int count2 = 0;
+		int position2 = 1;
+			while (position2 < dna.length) {
+		
+			//Look for start codon
+			for (int i = position2; i < dna.length; i = i + 3) {
+				if (i + 2 > dna.length - 1) {
+					i = dna.length;
+				}
+				else {
+					checkA[0] = dna[i];
+					checkA[1] = dna[i + 1]; 
+					checkA[2] = dna[i + 2];
+				}
+					position2 = i + 3;
+					if (checkA[0] == meth[0] && checkA[1] == meth[1] && checkA[2] == meth[2]) {
+						i = dna.length;
+				}
+					
+			}
+			
+			//Look for stop codon
+			
+			for (int i = position2; i < dna.length; i = i + 3) {
+				if (i + 2 > dna.length - 1) {
+					i = dna.length;
+				}
+				else {
+					checkA[0] = dna[i];
+					checkA[1] = dna[i + 1]; 
+					checkA[2] = dna[i + 2];
+				}
+				position2 = i + 3;
+				if ((checkA[0] == ochre[0] && checkA[1] == ochre[1] && checkA[2] == ochre[2]) || (checkA[0] == amber[0] && checkA[1] == amber[1] && checkA[2] == amber[2]) || (checkA[0] == opal[0] && checkA[1] == opal[1] && checkA[2] == opal[2])) {
+					i = dna.length;
+				}
+				else {
+					count2 = count2 + 1;
+				}
+			}
+			
+			}
+			
+			
+		//Ref 3
+			int count3 = 0;
+			int position3 = 2;
+			while (position3 < dna.length) {
+			
+			//Look for start codon
+			for (int i = position2; i < dna.length; i = i + 3) {
+				if (i + 2 > dna.length - 1) {
+					i = dna.length;
+				}
+				else {
+					checkA[0] = dna[i];
+					checkA[1] = dna[i + 1]; 
+					checkA[2] = dna[i + 2];
+				}
+					position3 = i + 3;
+					if (checkA[0] == meth[0] && checkA[1] == meth[1] && checkA[2] == meth[2]) {
+						i = dna.length;
+				}
+					
+			}
+			
+			//Look for stop codon
+			
+			for (int i = position3; i < dna.length; i = i + 3) {
+				if (i + 2 > dna.length - 1) {
+					i = dna.length;
+				}
+				else {
+					checkA[0] = dna[i];
+					checkA[1] = dna[i + 1]; 
+					checkA[2] = dna[i + 2];
+				}
+				position3 = i + 3;
+				if ((checkA[0] == ochre[0] && checkA[1] == ochre[1] && checkA[2] == ochre[2]) || (checkA[0] == amber[0] && checkA[1] == amber[1] && checkA[2] == amber[2]) || (checkA[0] == opal[0] && checkA[1] == opal[1] && checkA[2] == opal[2])) {
+					i = dna.length;
+				}
+				else {
+					count3 = count3 + 1;
+				}
+			}
+			
+			}
+		
+		if (count1 > count2 && count1 > count3) {
+			ans = 0;
+		}
+		if (count2 > count1 && count2 > count3) {
+			ans = 1;
+		}
+		if (count3 > count1 && count3 > count2) {
+			ans = 2;
+		}
 		
 		return ans;
 	}
