dd35a76fb6289ee322f1e179c3a2ae347417e157
sjbla <sjbla@Shane-LT.dhcp.wustl.edu>
2017-02-13 19:18:05 -0600

    Initial completion of exam1 (ExamTemplate.java)

--- exams/exam1/ExamTemplate.java
+++ exams/exam1/ExamTemplate.java
@@ -3,15 +3,47 @@ public class ExamTemplate {
 	
 	public static int[] question1() {
 		// feel free to modify the declaration of answer as necessary
-		int[] answer = new int[0];
+		int[] answer = new int[50];
 	
+		answer[0] = 3;
+		answer[1] = 0;
+		answer[2] = 2;
+		for (int i = 3; i < 50; i++) {
+			answer[i] = answer[i-2] + answer[i-3];
+		}
+		
+		
 		// don't change the return statement
 		return answer;
 	}
 	
 	public static boolean[] question2() {
 		// feel free to modify the declaration of answer as necessary
-		boolean[] answer = new boolean[0];
+		boolean[] answer = new boolean[50];
+		
+		int [] sieve = new int[50];
+			
+		for (int i = 0; i < sieve.length - 1; i++) {
+			sieve[i] = (i+2);
+		}
+
+		for (int k = 0; k < sieve.length; k++) {
+			for (int i = 2; i < sieve.length; i++) {
+				for (int j = i; j < sieve.length; j++){ 
+					if (sieve[j] % (i) == 0) {
+						sieve[j] = (0);
+					}	
+				}	
+			}
+		}
+		
+		answer[1] = true;
+		for (int i = 0; i < 50; i++)  {
+			if (sieve[i] != 0) {
+				answer[i+2] = true;
+				}
+		}
+	
 
 		// don't change the return statement
 		return answer;
@@ -23,6 +55,16 @@ public class ExamTemplate {
 		
 		// you may want to use your answer from question 1 in answering this question
 		int[] question1Answer = question1();
+		int sum = 0;
+		int i = 0;
+		
+		while (sum <= 1000) {
+			sum = sum + question1Answer[i];
+			i = i + 1;
+			if (sum <= 1000){
+				answer = answer + 1;
+			}
+		}
 		
 		// don't change the return statement
 		return answer;
@@ -30,11 +72,20 @@ public class ExamTemplate {
 	
 	public static String[] question4() {
 		// feel free to modify the declaration of answer as necessary
-		String[] answer = new String[0];
+		String[] answer = new String[50];
 		
 		// you may want to use your answer from questions 1 and 2 in answering this question
 		int[] question1Answer = question1();
 		boolean[] question2Answer = question2();
+		
+		for (int i = 0; i < 50; i++) {
+			if (question2Answer[i] == true) {
+				answer[i] = i + " is PRIME:\t" + question1Answer[i] + "%" + i + "==0";
+			}
+			else {
+				answer[i] = i + " is NONPRIME";
+			}
+		}
 		
 		// don't change the return statement
 		return answer;
