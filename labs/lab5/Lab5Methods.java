739493a594ad2dfb5e8c7267a009506d77f3904e
Chingling <Chingling@Shane-PC.resnet.wustl.edu>
2017-02-21 22:26:00 -0600

    Add JavaDoc comments to Assignment 5

--- labs/lab5/Lab5Methods.java
+++ labs/lab5/Lab5Methods.java
@@ -2,6 +2,7 @@ package lab5;
 
 public class Lab5Methods {
 
+/**Sum of the positive integers n + (n-2) + (n-4)...*/
 	public static int sumDownBy2(int n) {
 		int ans = n;
 		int check = n - 2;
@@ -14,6 +15,7 @@ public class Lab5Methods {
 		return ans;
 	}
 	
+/**Sum of 1 + 1/2 + 1/3 + ... + 1/(n-1) + 1/n*/
 	public static double harmonicSum(int n) {
 		double ans = 1;
 		int check = n;
@@ -26,7 +28,7 @@ public class Lab5Methods {
 		
 		return ans;
 	}
-	
+/**Sum 1 + 1/2 + 1/4 + 1/8 + ... + 1/Math.pow(2,k)*/
 	public static double geometricSum(int k) {
 		double ans = 1;
 		int check = k;
@@ -38,7 +40,7 @@ public class Lab5Methods {
 		}
 		return ans;
 	}
-
+/**Product of positive j and k values*/
 	public static int multPos(int j, int k) {
 		int ans = 0;
 		
@@ -48,7 +50,7 @@ public class Lab5Methods {
 		
 		return ans;
 	}
-	
+/**Product of j and k including sign*/
 	public static int mult(int j, int k) {
 		int ans = multPos(Math.abs(j), Math.abs(k));
 
@@ -58,7 +60,7 @@ public class Lab5Methods {
 		
 		return ans;
 	}
-	
+/**Value of n^k*/	
 	public static int expt(int n, int k) {
 		int ans = 1;
 		
