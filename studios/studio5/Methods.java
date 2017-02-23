08380b8f32f3b991ef2625c939d0ed31a51b6027
sjbla <sjbla@Shane-LT.dhcp.wustl.edu>
2017-02-23 14:56:48 -0600

    Completion of Studio 5 (Steven Lee, Evelyn is leaving, Shane Blair)

--- studios/studio5/Methods.java
+++ studios/studio5/Methods.java
@@ -19,8 +19,50 @@ public class Methods {
 	 * @return the product of the factors
 	 */
 	public static int mpy(int x, int y) {
-		return 0;  // FIXME
+		return x*y; 
+	}
+	
+	
+	/**
+	 * 
+	 * @param x one number
+	 * @param y another number
+	 * @param z yet another number
+	 * @return the average of the numbers
+	 */
+	public static double avg3(double x, double y, double z){
+		return (x+y+z)/3;
 	}
 
+	/**\
+	 * 
+	 * @param sumthing array of doubles
+	 * @return sum of array of doubles
+	 */
+	public static double sumArray(double[] sumthing){
+		double sum=0;
+		for(int i=0; i<sumthing.length;i++){
+			sum = sum + sumthing[i];
+		}
+		return sum;
+	}
+	/**
+	 * 
+	 * @param sumthing array of doubles
+	 * @return average of array of doubles
+	 */
+	public static double average(double[] sumthing){
+		double sum = sumArray(sumthing);
+		return sum/sumthing.length;
+	}
+	
+	/**
+	 * 
+	 * @param s String of word
+	 * @return piglatin version of word
+	 */
+	public static String pig(String s){
+		return s.substring(1) + s.substring(0,1) +"ay";
+	}
 
 }
--- studios/studio5/MethodsTest.java
+++ studios/studio5/MethodsTest.java
@@ -11,7 +11,7 @@ public class MethodsTest {
 		//
 		// Try something simple
 		//
-		assertEquals(7, Methods.sum(3, 4));
+		assertEquals(-8, Methods.sum(-4, -4));
 		//
 		// Try something fancier
 		//
@@ -22,8 +22,8 @@ public class MethodsTest {
 	
 	@Test
 	public void thisTestShouldFail() {
-		assertEquals(1, Methods.mpy(0,3));
-		assertEquals(1, Methods.mpy(2, 5));
+		assertEquals(1, Methods.mpy(1,1));
+		assertEquals(10, Methods.mpy(2, 5));
 	}
 	
 	@Test
