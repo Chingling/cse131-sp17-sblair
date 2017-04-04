693208c55b80a863ed6576ffaf352db3e19cc4e7
Chingling <sj.blair@yahoo.com>
2017-04-03 20:03:11 -0500

    Update to Assignment 8 to use stupid forced Iterator
    (because creativity is apparently not important)

--- labs/lab8/Test.java
+++ labs/lab8/Test.java
@@ -1,5 +1,6 @@
 package lab8;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,11 +9,18 @@ public class Test {
 
 	public static void main(String[] args) {
 		Polynomial p = new Polynomial();
-		p.addTerm(4);
-		p.addTerm(-7);
-		p.addTerm(0);
-		p.addTerm(11);
-		System.out.println(p);
+		LinkedList<Double> list = new LinkedList<Double>();
+		list.add(7.0);
+		list.add(5.0);
+		list.add(-2.0);
+		list.add(5.0);
+		System.out.println(list);
+		Iterator<Double> iter = list.descendingIterator();
+		System.out.println(iter.next());
+		System.out.println(iter.next());
+		System.out.println(iter.next());
+		System.out.println(iter.next());
+//		System.out.println(iter.next());
 	}
 
 }
